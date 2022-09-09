39b42f71a4651980d1f3a575300f03fbd48a4f4a
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-09-09 05:15:45 -0700

    puts c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
  * * Description: using the main function
  * * this program prints "Programming is like building a multilingual puzzle
  * * Return: 0
- * */
+*/
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
