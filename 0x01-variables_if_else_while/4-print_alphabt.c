2a3a5fbe28ad4876c71ea779b54ac8469aa22ed4
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-09-09 20:45:43 -0700

    print alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char letter; /*Decarling statement*/
+/*for letter*/
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+if (letter == 'q') /*continue if is q*/
+continue;
+else if (letter == 'e')/*continue if is q*/
+continue;
+putchar(letter);/*print letter*/
+}
+return (0);
+}
