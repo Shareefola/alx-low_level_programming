da549018650b417dab691af591a50effab48fc9d
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-09-13 16:37:06 -0700

    Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -6,21 +6,22 @@
 
  *Return: 1 if +, 0 if 0, and -1 if -
  */
+
 int print_sign(int n)
 {
 	if (n > 0)
 	{
 		_putchar('+');
-		return(1);
+		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 	else
 	{
 		_putchar('0');
-		return;(0);
+		return (0);
 	}
 }
