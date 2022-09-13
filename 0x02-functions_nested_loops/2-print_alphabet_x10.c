c4509616ee05929f71c653976209707d49a82159
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-09-13 15:38:00 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - a function that prints 10 times the alphabet
+ *
+ *Return: Always(0)
+ */
+
+void print_alphabet_x10(void)
+{
+	int alphabet_count = 0;
+
+	while (alphabet_count < 10)
+
+	{
+		char ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+
+		alphabet_count++;
+		_putchar('\n');
+	}
+}
