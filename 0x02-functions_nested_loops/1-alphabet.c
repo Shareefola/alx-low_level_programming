16c197da17c054a3acffdc7fa3a10cab2d3d1dab
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-09-13 15:18:43 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_alphabet -> prints the lowercase
+ *
+ *Return: void
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
+
