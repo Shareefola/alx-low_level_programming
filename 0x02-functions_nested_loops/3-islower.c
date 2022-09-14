d09daeee58a3372c5b2ef3451536c69ee3a55fb9
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-09-13 23:31:35 -0700

    Is Lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,7 +3,7 @@
  *_islower - prints 1 or 0 depending on input
  *@c: first parameter
  *Description: prints all lowercase letters
- * * Return: Always(0)
+ *Return: Always(0)
  */
 
 int _islower(int c)
