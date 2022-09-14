4282a27ae24c77ec31466856b9a3ae89900a9bd7
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-09-13 17:08:03 -0700

    24 Hours

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,8 @@ void jack_bauer(void)
 			_putchar(i / 10 + 48);
 			_putchar(i % 10 + 48);
 			_putchar(':');
-			_putchar(j / 10 +48);
-			_putchar(j % 10 +48);
+			_putchar(j / 10 + 48);
+			_putchar(j % 10 + 48);
 			_putchar('\n');
 		}
 	}
