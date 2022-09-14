4d1cb21b2e892e688dae51d8f42ca99dc9db0105
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-09-13 23:00:07 -0700

    Is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
-/** 
- * _islower - prints 1 or 0 depending on input 
- * @c: first parameter 
- * 
- * Description: prints all lowercase letters 
- * Return: Always(0). 
+/**
+ *_islower - prints 1 or 0 depending on input
+ *@c: first parameter
+ *Description: prints all lowercase letters
+ * * Return: Always(0)
  */
 
 int _islower(int c)
