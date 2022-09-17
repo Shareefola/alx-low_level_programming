570c47bb2c5493f6a7a6947fd667e2fd155d8b72
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-09-16 19:09:14 -0700

    Debugging

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,10 @@
 
 #include <stdio.h>
 
+int largest_number(int, int, int);
+void print_remaining_days(int, int, int);
+int convert_day(int, int);
+
 void positive_or_negative(int i);
 
 #endif /* MAIN_H */
