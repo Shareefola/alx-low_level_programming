8cecd180372c0a76df914392eca63e8b268840f7
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-09-22 10:07:50 -0700

    strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
- *_strncat :  concatenate two strings
- *@dest: string
- *@src: string
- *@n: number of elements to concatenate in
- *Return: pointer to resulting `dest`
+ * _strncat :  concatenate two strings
+ * @dest: string
+ * @src: string
+ * @n: number of elements to concatenate in
+ *
+ * Return: pointer to resulting `dest`
  */
 
 char *_strncat(char *dest, char *src, int n)
