207a7115bb44d93f860be672c793d36da927ca73
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-09-26 08:45:31 -0700

    Memeset

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef _HOLBERTON_H_
-#define _HOLBERTON_H_
+#ifndef MAIN_H
+#define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
