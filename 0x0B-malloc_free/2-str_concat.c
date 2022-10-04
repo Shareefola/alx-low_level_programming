58a91fef9621cdefb410cc34ef0881491c994682
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-10-04 02:10:44 -0700

    Malloc Free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 
 /**
- * str_concat - concatenates two strings.
- * @s1: first string.
- * @s2: second string.
+ *str_concat - concatenates two strings.
+ *@s1: first string.
+ *@s2: second string.
  *
- * Return: pointer of an array of chars
+ *Return: pointer of an array of chars
  */
-
 char *str_contat(char *s1, char *s2)
 {
 	char *strout;
