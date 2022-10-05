069d090fa7cb86e6a3c6074a3df3910bea480e25
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-10-05 08:45:03 -0700

    Malloc Free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -56,7 +56,7 @@ char **strtow(char *str)
 			if(c)
 			{
 				end = i;
-				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				tmp = (char*) malloc(sizeof(char) * (c + 1));
 				if (tmp == NULL)
 					return (NULL);
 				while (start < end)
