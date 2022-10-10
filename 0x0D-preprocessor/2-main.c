6ad759760026a81e7e7e3bb7842dc4ef6d1693a8
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-10-10 08:23:48 -0700

    Preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints the name of the file program was compiled from
- * Return: Always 0
+ *main - prints the name of the file program was compiled from
+ *Return: Always 0
  */
 
 int main(void)
