4a4123fba1bdba212a2a3e6ba3cb4dab1a6986e5
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-10-10 15:23:24 -0700

    Structures

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,50 +1,24 @@
-#include "dog.h"
-#include <stdlib.h>>
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
 
 /**
- *new_dog - new dog
- *@name: name's dog
- *@age: age's dog
- *@owner: owner's dog
- *Return: newdog
+ *struct dog - Struct dog
+ *@name: name
+ *@age: age
+ *@owner:  owner
+ *Description: This struct is for dog
  */
-
-dog_t *new_dog(char *name, float age, char *owner)
+struct dog
 {
+	char *name;
+	float age;
+	char *owner;
+};
 
-	int i = 0, j = 0, k;
-	dog_t *doge;
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 
-	while (name[i] != '\0')
-		i++;
-	while (owner[j] != '\0')
-		j++;
-	doge = malloc(sizeof(dog_t));
-	if (doge == NULL)
-	{
-		free(doge);
-		return (NULL);
-	}
-	doge->name = malloc(i * sizeof(doge->name));
-	if (doge->name == NULL)
-	{
-		free(doge->name);
-		free(doge);
-		return (NULL);
-	}
-	for (k = 0; k <= i; k++)
-		doge->name[k] = name[k];
-	doge->age = age;
-	doge->owner = malloc(j * sizeof(doge->owner));
-	if (doge->owner == NULL)
-	{
-		free(doge->owner);
-		free(doge->name);
-		free(doge);
-		return (NULL);
-	}
-	for (k = 0; k <= j; k++)
-		doge->owner[k] = owner[k];
-	return (doge);
-}			
+#endif /*DOG_H*/
