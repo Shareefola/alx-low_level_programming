17c6d4d0fd97ac45525a5e0d88e88018af974f4f
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-10-11 14:31:28 -0700

    Structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,10 +2,10 @@
 #define FILE_DOG
 
 /**
- * struct dog - structure dog.
- * @name: atribute pointer to char name's dog
- * @age: atribute float age's dog
- * @owner: atribute pointer to char owner's dog
+ *struct dog - Struct dog.
+ *@name: atribute pointer to char name's dog
+ *@age: atribute float age's dog
+ *@owner: atribute pointer to char owner's dog
  */
 
 struct dog
