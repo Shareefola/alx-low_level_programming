cca9d66aa1678a384192cb9329c9b92954197cab
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-10-12 15:45:39 -0700

    Function and Pointers

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -6,31 +6,36 @@
  *@argv: argument vector
  *Return: Always 0
  */
-int main(int argc, char *argc[])
+int main(int argc, char *argv[])
 {
-	char *opc = (char *) main;
-	int i, nbytes;
+	int bytes, i;
+	char *arr;
 
-	if (argc != 2)
+	if (argc !=2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
 
-	nbytes = atoi(argv[1]);
+	bytes = atoi(argv[1]);
 
-	if (nbytes < 0)
+	if (bytes < 0)
 	{
 		printf("Error\n");
 		exit(2);
 	}
 
-	for (i = 0; i < nbytes; n++)
+	arr = (char *)main;
+
+	for (i = 0; i < bytes; i++)
 	{
-		printf("%02x", opc[i] & 0xFF);
-		if (1 != nbytes - 1)
-			printf("");
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("02hhx ", arr[i]);
 	}
-	printf("\n");
 	return (0);
 }
+
