5596ee7771efe48311031716fed9eaaa87aaad34
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-10-12 13:55:21 -0700

    Function Pointers

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ *get_op_func - identify function
+ *@s: function
+ *Return: int
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", po_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+	while (i < 5)
+	{
+		if (s[0] == ops[i].op[0])
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "3-calc.h"
+/**
+ *op_add - Struct op
+ *@a: The Operation
+ *@b: The function associated
+ *Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ *op_sub - Struct op
+ *@a: The operator
+ *@b: The function associated
+ *Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - Struct op
+ *@a: The operator
+ *@b: The function associated
+ *Return: int
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - Struct op
+ *@a: The Operator
+ *@b: The funtion associated
+ *Return: int
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	result (a / b);
+}
+/**
+ *op_mod - Struct op
+ *@a: The operator
+ *@b: The function associated
+ *Return
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("100\n");
+		exit(100);
+	}
+	return (a % b);
+}
