e314f6736573ecc12f1d665c6be7bece4dd9a579
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-10-11 22:00:51 -0700

    Structures Typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,15 +9,25 @@
 
 void print_dog(struct dog *d)
 {
-	if (d = 0)
-		return;
-		if (d->name = 0)
-			printf("Name: (nil)\n");
-		else
+	if (d)
+	{
+		if(d->name != NULL)
+		{
 			printf("Name: %s\n", d->name);
-		printf("Age: %f\n", d->age);
-		if (d->owner == 0)
-			printf("Owner: (nil)\n");
+		}
 		else
-			printf("owner: %s\n", d->owner);
+		{
+			printf("Name: (nil)\n");
+		}
+		printf("Age: %f\n", d->age);
+		if (d->owner)
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+		else
+		{
+			printf("Owner: (nil)\n");
+		}
+	}
 }
+
