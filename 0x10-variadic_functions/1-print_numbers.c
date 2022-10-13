2dc251fc70c5c9f78366c1ffcc9d82d1279ce7c2
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-10-13 14:03:28 -0700

    Variadic functions

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,7 +14,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d" va_arg(valist, int));
+		printf("%d", va_arg(valist, int));
 		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
