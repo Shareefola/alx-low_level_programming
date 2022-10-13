61f4a4051be66b17b295a027b72d7a38969cefd3
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-10-13 14:10:19 -0700

    Variadic functions

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -7,7 +7,7 @@
  *
  *Return: no return
  */
-void print_strings(const char *separator, const unsigned int n, ...)
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list valist;
 	unsigned int i;
