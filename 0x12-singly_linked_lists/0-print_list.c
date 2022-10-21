fdcd6dd684d81b11281ff683f2e00905b3e5a21a
Ogunyemi Sheriff <ogunyemisheriff31@gmail.com>
2022-10-21 13:37:52 -0700

    Singly Linked

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 
 /**
- * print_list - prints all the elements of a lists
- * @h: singly linked list
- * Return: number of elements in the list.
+ *print_list - prints all the elements of a lists
+ *@h: singly linked list
+ *Return: number of elements in the list.
  */
 
 size_t print_list(const list_t *h)
